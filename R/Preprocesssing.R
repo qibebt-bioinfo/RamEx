e4e1ec22b524b99570998f48eafd7328918ed696
qibebt-bioinfo <46369225+qibebt-bioinfo@users.noreply.github.com>
2025-01-31 18:42:50 +0800

    Update Preprocesssing.R

--- R/Preprocesssing.R
+++ R/Preprocesssing.R
@@ -394,7 +394,7 @@ gpupre_spike_matrix <- function(all_data, device = NULL) {
   all_spc_1 <- matrix(conv_result, nrow = nrow(all_spc) - 2, ncol = ncol(all_spc) - 10, byrow = TRUE)
 
   slope_inds_2 <- which(all_spc_1 > 10 * apply(all_spc, 1, max), arr.ind = TRUE)
-  slope_inds_2 <- which(all_spc_1 > 10 * apply(all_spc, 1, max), arr.ind = TRUE)
+  #slope_inds_2 <- which(all_spc_1 > 10 * apply(all_spc, 1, max), arr.ind = TRUE)
 
   slope_inds <- slope_inds_2
 
